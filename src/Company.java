1a202455db2711cdad24fd787c80b7d64598b757
Damith122 <damithakshitha122git@gmail.com>
2023-10-23 12:18:06 +0530

    Method Ovrloading

--- src/Company.java
+++ src/Company.java
@@ -7,7 +7,7 @@ public class Company {
 
     }
 
-    public void get(int id, String name) {
+    public void  get (int id, String name) {
 
         System.out.println("Company ID : " + id);
         System.out.println("Company Name : " + name);
